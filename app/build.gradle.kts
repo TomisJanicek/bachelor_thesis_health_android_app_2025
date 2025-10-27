a0b2e9aafbbb1fb57ec5567825b03d833bb42fc3
Tomis_Janicek <tomisjanicek@gmail.com>
2025-10-27 13:03:10 +0100

    Nastavení Gradle
    Kompletní nastavení Gradle co bych asi měl potřebovat

---
+++ app/build.gradle.kts
@@ -0,0 +1,158 @@
+import java.util.Properties
+
+plugins {
+        alias(libs.plugins.android.application)
+        alias(libs.plugins.jetbrains.kotlin.android)
+        kotlin("kapt")
+        id("kotlin-kapt") // nebo id("com.google.devtools.ksp")
+        id("dagger.hilt.android.plugin")
+}
+
+val properties = Properties()
+val propertiesFile = project.rootProject.file("local.properties")
+if (propertiesFile.exists()) {
+    properties.load(propertiesFile.inputStream())
+}
+
+android {
+    namespace = "cz.tomasjanicek.bp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "cz.tomasjanicek.bp"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.image.labeling.common)
+    implementation(libs.image.labeling.default.common)
+    implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.ui.test.junit4.android)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    kapt(libs.hilt.kapt)
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.okhtt3)
+    // Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ktx)
+    implementation(libs.navigation.compose)
+
+    implementation(libs.googlemap)
+    implementation(libs.googlemap.compose)
+    implementation(libs.googlemap.foundation)
+    implementation(libs.googlemap.utils)
+    implementation(libs.googlemap.widgets)
+    implementation(libs.googlemap.compose.utils)
+    implementation (libs.play.services.location)
+
+
+    implementation(libs.lifecycle)
+    implementation(libs.room.ktx)
+    implementation(libs.room.viewmodel)
+    implementation(libs.room.lifecycle)
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler.kapt)
+
+    implementation(libs.material)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.coil.compose)
+
+    implementation (libs.gson)
+
+    implementation (libs.image.labeling)
+    implementation (libs.vision.common)
+
+    implementation (libs.converter.gson)
+
+    // Hilt testing
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.57.2")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.57.2")
+
+    //  Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(kotlin("script-runtime"))
+
+    // Testing
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.core.testing)
+
+    // Android Testing
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+
+    // Espresso
+    androidTestImplementation(libs.androidx.espresso.core) {
+        exclude(group = "androidx.test", module = "runner")
+        exclude(group = "androidx.test", module = "rules")
+    }
+
+    // Hilt Testing
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.kapt)
+
+    testImplementation ("io.mockk:mockk:1.14.6")
+
+    // MockK pro instrumentované testy (pokud ho plánuješ používat i v androidTest)
+    androidTestImplementation ("io.mockk:mockk-android:1.14.6")
+
+}
+
+// For Hilt
+kapt {
+    correctErrorTypes = true
+}
---
+++ app/src/main/java/cz/tomasjanicek/bp/HealthApplication.kt
@@ -0,0 +1,8 @@
+package cz.tomasjanicek.bp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class HealthApplication : Application()
